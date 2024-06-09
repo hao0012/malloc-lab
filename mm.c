2d502b42c507743a549632505bfe1999261db40d
hao0012 <709704094@qq.com>
2024-06-09 17:40:42 +0800

    more list

--- mm.c
+++ mm.c
@@ -75,9 +75,13 @@ team_t team = {
 #define GET_SUCC(bp)  ((void *)GET(((char *)bp + WSIZE)))
 #define PUT_SUCC(bp, succ) (*((unsigned int *)((char *)(bp) + WSIZE)) = (succ))
 
-#define LIST_COUNT 9
+#define LIST_COUNT 10
 #define INT_MAX 0x7fffffff
-static int threshold[LIST_COUNT] = {32, 64, 128, 256, 512, 1024, 2048, 4096, INT_MAX};
+static int threshold[LIST_COUNT] = {
+    1 >> 4, 1 >> 5, 1 >> 6, 1 >> 7, 1 >> 8, 1 >> 9, 1 >> 10, 1 >> 11, 1 >> 12, 
+    // 1 >> 13, 1 >> 14, 1 >> 15, 1 >> 16, 1 >> 17, 1 >> 18, 1 >> 19, 1 >> 20, 1 >> 21, 
+    // 1 >> 22, 1 >> 23, 1 >> 24, 1 >> 25, 1 >> 26, 1 >> 27, 1 >> 28, 1 >> 29, 1 >> 30, 
+    INT_MAX};
 int get_index(int size) {
     for (int i = 0; i < LIST_COUNT; i++) {
         if (size < threshold[i]) return i;
